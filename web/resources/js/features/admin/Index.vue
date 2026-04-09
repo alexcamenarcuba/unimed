054c2b1684529aeec4722e7cdc632359805e684b
Alex Vinicius <acuba@unimedpr.coop.br>
2026-04-09 13:26:54 -0300

    instalando primevue

--- web/resources/js/features/admin/Index.vue
+++ web/resources/js/features/admin/Index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <h1>{{ mensagem }}</h1>
-    <p>Laravel + Inertia.js + Vue 3 funcionando! 🚀</p>
-  </div>
+    <div>
+        <h1>{{ mensagem }}</h1>
+        <p>Laravel + Inertia.js + Vue 3 funcionando! 🚀</p>
+        <h1>Teste PrimeVue</h1>
+
+        <InputText v-model="nome" placeholder="Digite seu nome" class="mb-4" />
+
+        <Button
+            label="Enviar"
+            icon="pi pi-check"
+            @click="() => alert('Olá, ' + nome)"
+        />
+    </div>
 </template>
 
 <script setup>
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import { ref } from "vue";
+
+const nome = ref("");
 defineProps({
-  mensagem: String,
+    mensagem: String,
 });
 </script>
