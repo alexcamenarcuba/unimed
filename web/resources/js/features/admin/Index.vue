b251ae58d7931b6ab365e8a7714cbaaf4dbd4f71
Alex Vinicius <acuba@unimedpr.coop.br>
2026-04-09 13:22:54 -0300

    instalando o inertia

---
+++ web/resources/js/features/admin/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{ mensagem }}</h1>
+    <p>Laravel + Inertia.js + Vue 3 funcionando! 🚀</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  mensagem: String,
+});
+</script>
